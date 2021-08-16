9b470c51c68f09bb4962bf7e659d7f5f150d9af6
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-16 23:55:08 +0200

    Include style.css file

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   devServer: {
     contentBase: './dist',
@@ -15,5 +16,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
