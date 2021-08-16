2a5ca8a91918757f6692a943c6e66d6c612e88b7
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-16 17:46:25 +0200

    Create tasks array in index.js file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const tasks = [
+  {
+    description: "Clean the bathroom",
+    completed: false,
+    index: 0,
+  },
+  {
+    description: "Ride a bicycle",
+    completed: false,
+    index: 0,
+  },
+  {
+    description: "Go shopping",
+    completed: false,
+    index: 0,
+  }
+]
