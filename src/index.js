0681fe5e373c0441225223f127e02969a058938f
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-16 22:10:22 +0200

    Create populate list function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const tasks = [
+const taskList = [
   {
     description: "Clean the bathroom",
     completed: false,
@@ -7,11 +7,26 @@ const tasks = [
   {
     description: "Ride a bicycle",
     completed: false,
-    index: 0,
+    index: 1,
   },
   {
     description: "Go shopping",
     completed: false,
-    index: 0,
+    index: 2,
   }
-]
+];
+
+function populateList () {
+  const tasks = document.getElementById('tasks');
+  tasks.style.display = 'flex';
+  const taskElement = document.createElement('li');
+  const square = document.createElement('i');
+  const desc = document.createElement('p');
+  taskList.forEach((task) => {
+    task.completed ? square.classList.add('far','fa-square') : square.classList('fas', 'fa-check');
+     desc.innerHTML = task.description;
+    taskElement.style.order = 'index';
+    tasks.innerHTML += square + desc 
+  })
+}
+populateList();
