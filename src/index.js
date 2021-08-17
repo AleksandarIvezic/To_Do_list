aa84c826933d229c3f61de31ad03426ee6b0a679
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-17 10:39:02 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,35 +2,36 @@ import './style.css';
 
 const taskList = [
   {
-    description: "Clean the bathroom",
+    description: 'Clean the bathroom',
     completed: false,
     index: 2,
   },
   {
-    description: "Ride a bicycle",
+    description: 'Ride a bicycle',
     completed: true,
     index: 1,
   },
   {
-    description: "Go shopping",
+    description: 'Go shopping',
     completed: false,
     index: 0,
-  }
+  },
 ];
 
 const tasks = document.getElementById('tasks');
-function populateList () {
+function populateList() {
   taskList.forEach((task) => {
     const taskElement = document.createElement('li');
     const left = document.createElement('span');
     const right = document.createElement('span');
     const square = document.createElement('i');
     const desc = document.createElement('p');
-    task.completed ? square.classList.add('fas', 'fa-check') : square.classList.add('far','fa-square') ;
-     desc.innerHTML = task.description;
+    // eslint-disable-next-line no-unused-expressions
+    task.completed ? square.classList.add('fas', 'fa-check') : square.classList.add('far', 'fa-square');
+    desc.innerHTML = task.description;
     taskElement.style.order = task.index;
     left.innerHTML = square.outerHTML + desc.outerHTML;
-    right.innerHTML = '<i class="fas fa-ellipsis-v"></i>'
+    right.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
     taskElement.innerHTML = left.outerHTML + right.outerHTML;
     tasks.appendChild(taskElement);
   });
@@ -38,6 +39,5 @@ function populateList () {
   clearTasks.setAttribute('id', 'clearTasks');
   clearTasks.innerText = 'Clear all completed';
   tasks.appendChild(clearTasks);
-};
+}
 populateList();
-console.log(tasks);
