cc413d8f9d08dabaf9934db679842f0c8c1b9574
Linux User <m_matongo@icloud.com>
2021-08-25 14:02:50 +0000

    add second test using expect.arrayContaining

--- src/addRemove.test.js
+++ src/addRemove.test.js
@@ -1,10 +1,16 @@
 import { addTask, removeTask } from './crud';
 
 describe('Test if addTask function', () => {
+  const tasksList = [];
+
   it('Add task to taskList', () => {
-    const tasksList = [];
     const description = 'Task 1';
     addTask(tasksList, description);
     expect(tasksList).toEqual([{ description: 'Task 1', completed: false, index: 0 }]);
+  });
+  it('Add another task to taskList', () => {
+    const description = 'Task 2';
+    addTask(tasksList, description);
+    expect(tasksList).toEqual(expect.arrayContaining([{ description: 'Task 2', completed: false, index: 1 }]));
   });
 });
