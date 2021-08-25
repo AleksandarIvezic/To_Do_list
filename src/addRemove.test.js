17f361103d1ab0ef7f3e5cfdf261eb9e098ffd25
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-25 13:41:13 +0200

    Refactor add test

--- src/addRemove.test.js
+++ src/addRemove.test.js
@@ -5,6 +5,6 @@ describe('Test if addTask function', () => {
     const tasksList = [];
     const description = 'Task 1';
     addTask(tasksList, description);
-    expect(tasksList).toBe([{description: 'Task 1', completed: false, index: 0}]);
+    expect(tasksList).toEqual([{ description: 'Task 1', completed: false, index: 0 }]);
   });
 });
