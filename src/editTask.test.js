0b57326bfd144359b90ca5a7e10f7e5db620d46c
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-25 16:43:10 +0200

    Create editTask.test.js file

---
+++ src/editTask.test.js
@@ -0,0 +1,12 @@
+// import { it } from "@jest/globals";
+// import { editTask } from "./crud";
+
+// describe ('Test editTask', () => {
+//   const descMock = document.createElement('p');
+//   spy.mockReturnValue(descMock);
+//   console.log(spy.mockReturnValue(descMock));
+//   it('check if input element is added to descMock', {
+//     const task = {description: 'Task 1', completed: false, index: 0 };
+//     expect( editTask(descMock, task)).
+//   });
+// });
