a31c031a9ddead64f432a79e73b994f79ee68efa
Matongo Mulindi <mmatongo_@hotmail.com>
2021-08-26 16:03:36 +0200

    add updateTask test

--- src/__tests__/editTask.test.js
+++ src/__tests__/editTask.test.js
@@ -1,8 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-import { expect, it } from '@jest/globals';
-import { editTask } from '../crud';
+import { describe, expect, it } from '@jest/globals';
+import { editTask, updateTask } from '../crud';
 
 describe('Test editTask', () => {
   document.body.innerHTML = '<div>'
@@ -20,5 +20,26 @@ describe('Test editTask', () => {
     const event = new Event('click');
     desc.dispatchEvent(event);
     expect(desc.innerHTML).toEqual('<input id="0">');
+  });
+});
+describe('Test updateTask', () => {
+  document.body.innerHTML = '<div>'
+   + '  <ul>'
+   + '    <li> <span><p id = "desc-0"><input id="0"></p></span>'
+   + '  </ul>'
+   + '</div>';
+  const descInput = document.getElementById('0');
+  const desc = document.getElementById('desc-0');
+  const task = { description: 'Task 1', completed: false, index: 0 };
+  it('Check if updateTask is functional', () => {
+    descInput.value = 'Task 2';
+    descInput.addEventListener('focusout', (e) => {
+      updateTask(e, task);
+    });
+
+    const event = new Event('focusout');
+    descInput.dispatchEvent(event);
+
+    expect(desc.innerText).toBe('Task 2');
   });
 });
