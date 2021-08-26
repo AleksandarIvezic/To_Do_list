4044b875b1e26df57845a8a7659b43be34adcacd
Matongo Mulindi <mmatongo_@hotmail.com>
2021-08-26 15:30:49 +0200

    reposition +

--- src/__tests__/editTask.test.js
+++ src/__tests__/editTask.test.js
@@ -5,12 +5,11 @@ import { expect, it } from '@jest/globals';
 import { editTask } from '../crud';
 
 describe('Test editTask', () => {
-  document.body.innerHTML = 
-   '<div>' +
-   '  <ul>' +
-   '    <li> <span><p id = "desc-0">Task 1</p></span>' +
-   '  </ul>' +
-   '</div>';
+  document.body.innerHTML = '<div>'
+   + '  <ul>'
+   + '    <li> <span><p id = "desc-0">Task 1</p></span>'
+   + '  </ul>'
+   + '</div>';
   const desc = document.getElementById('desc-0');
   const task = { description: 'Task 1', completed: false, index: 0 };
   // spy.mockReturnValue(descMock);
